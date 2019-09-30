1c496a9968c44673c70ff2537ca3143b7aa24c7d
NatalieKu <b86103018@ntu.edu.tw>
2019-09-30 11:16:00 +0800

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by MEI KU on 2019/9/30.
+//  Copyright © 2019 Natalie KU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
